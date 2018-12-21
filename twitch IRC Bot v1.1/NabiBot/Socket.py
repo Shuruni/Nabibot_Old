3aedb46aa3fe8005a89b003fae444984af6785b8
Shuruni <speedster708@gmail.com>
2018-12-21 04:55:09 -0600

    add v1.1

---
+++ twitch IRC Bot v1.1/NabiBot/Socket.py
@@ -0,0 +1,19 @@
+import socket
+from Settings import *
+"""online standard version"""
+def openSocket():
+	s = socket.socket()
+	s.connect((HOST,PORT))
+	s.send("PASS " + PASS + "\r\n")
+	s.send("NICK " + NICK + "\r\n")
+	s.send("JOIN #" + CHANNEL + "\r\n")
+	return s
+	
+def sendMessage(s, message):
+	messageTemp = "PRIVMSG #" + CHANNEL + " :" + message + "\r\n"
+	s.send(messageTemp.encode())
+	print("Sent: " + messageTemp)
+
+# """offline console testing version"""
+# def sendMessage(s, message):
+	# print message
